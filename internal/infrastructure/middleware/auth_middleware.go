8fb7a2fa608701b09ddfde2673087c82d037b98e
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-20 00:39:41 +0700

    feat: made jwt middleware validate data

--- internal/infrastructure/middleware/auth_middleware.go
+++ internal/infrastructure/middleware/auth_middleware.go
@@ -3,12 +3,13 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"ordent-test/internal/infrastructure/repository"
 	"ordent-test/pkg/auth"
 	"ordent-test/pkg/utils"
 	"strings"
 )
 
-func AuthMiddleware() gin.HandlerFunc {
+func AuthMiddleware(repo repository.UserRepository) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		header := ctx.GetHeader("Authorization")
 
@@ -26,6 +27,17 @@ func AuthMiddleware() gin.HandlerFunc {
 		token := strings.Replace(header, "Bearer ", "", 1)
 
 		user, err := auth.ParseJWT(token)
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, utils.NewErrorResponse(
+				"Unauthorized",
+				http.StatusUnauthorized,
+				"Invalid token",
+			))
+			return
+		}
+
+		user, err = repo.FindBySecureID(user.SecureID)
 
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, utils.NewErrorResponse(
--- internal/infrastructure/repository/user_repository.go
+++ internal/infrastructure/repository/user_repository.go
@@ -8,6 +8,7 @@ import (
 type UserRepository interface {
 	Create(user *model.User) (*model.User, error)
 	FindByEmail(email string) (*model.User, error)
+	FindBySecureID(secureID string) (*model.User, error)
 }
 
 type userRepository struct {
@@ -32,6 +33,18 @@ func (r *userRepository) FindByEmail(email string) (*model.User, error) {
 	var user model.User
 
 	err := r.db.Where("email = ?", email).First(&user).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) FindBySecureID(secureID string) (*model.User, error) {
+	var user model.User
+
+	err := r.db.Where("secure_id = ?", secureID).First(&user).Error
 
 	if err != nil {
 		return nil, err

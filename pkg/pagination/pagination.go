c2fb2ac83a4281d00f0ac6e097af6dab88a13d77
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-20 03:47:19 +0700

    feat: pagination utility model

---
+++ pkg/pagination/pagination.go
@@ -0,0 +1,32 @@
+package pagination
+
+import (
+	"gorm.io/gorm"
+)
+
+type Pagination struct {
+	Page  *int `form:"page,omitempty" binding:"omitempty,min=1"`
+	Limit *int `form:"limit,omitempty" binding:"omitempty,min=1,max=50"`
+}
+
+func (p *Pagination) Paginate() func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset((p.GetPage() - 1) * p.GetLimit()).Limit(p.GetLimit())
+	}
+}
+
+func (p *Pagination) GetPage() int {
+	if p.Page == nil {
+		return 1
+	}
+
+	return *p.Page
+}
+
+func (p *Pagination) GetLimit() int {
+	if p.Limit == nil {
+		return 5
+	}
+
+	return *p.Limit
+}

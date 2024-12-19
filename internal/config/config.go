8a1286cd91b51d521e2885bf8178f776454be88c
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-19 13:38:16 +0700

    feat: added .env reader

---
+++ internal/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type Config struct {
+	ServerPort string
+}
+
+var config *Config
+
+func Load() *Config {
+	if config != nil {
+		return config
+	}
+
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file: " + err.Error())
+	}
+
+	config := &Config{
+		ServerPort: getEnv("SERVER_PORT"),
+	}
+
+	return config
+}
+func getEnv(key string) string {
+	v := os.Getenv(key)
+
+	if v == "" {
+		panic("missing env var: " + key)
+	}
+
+	return v
+}
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"ordent-test/internal/config"
 	"ordent-test/internal/router"
 )
 
 func main() {
-	_ = router.NewRouter()
+	cfg := config.Load()
+
+	r := router.NewRouter()
+
+	err := r.Run(fmt.Sprintf(":%s", cfg.ServerPort))
+
+	if err != nil {
+		panic(err)
+	}
 }
